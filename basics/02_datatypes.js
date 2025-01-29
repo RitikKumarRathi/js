4ab063c314eb682d3a5d0c3916ec9123181a832b
RitikKumarRathi <ritikkumarrathi_co21a6_49@dtu.ac.in>
2025-01-29 11:14:33 +0530

    add datatypes and conversion

---
+++ basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) //we are using node.js, not browser
+
+let name = "Ritik"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 pow 53
+//bitint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not assigned value
+//symbol => unique
+
+//object
+
+console.log(typeof age) //number
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score = null
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber) //NaN
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+
+// 1 => bolean - true  0 => bolean - false
+// "" => boolean - false  "basn" => bolean - true
+
